beccf9fb69bd305727e7b6fbd1b61454369a6fea
cinthiaams <cinthiaams@gmail.com>
2025-03-11 15:10:09 +0000

    Adding changes in main and game

---
+++ code/Game.py
@@ -0,0 +1,21 @@
+import pygame as pg
+
+from code.Menu import Menu
+
+
+class Game:
+    def __init__(self):
+        pg.init()
+        self.window = pg.display.set_mode(size=(600, 480))
+
+    def run(self):
+        while True:
+            menu = Menu(self.window)
+            menu.run()
+            pass
+
+            # check for all events
+            # for event in pg.event.get():
+            #    if event.type == pg.QUIT:
+            #        pg.quit()  # close window
+            #        quit()  # end pygame
